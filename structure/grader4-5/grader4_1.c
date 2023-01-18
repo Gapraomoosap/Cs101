103250de30d86d0ca6bb63b9edd27f89f1448b59
Gapraomoosap <peelasin2544@gmail.com>
2023-01-18 15:50:19 +0700

    alt shift F

--- structure/grader4-5/grader4_1.c
+++ structure/grader4-5/grader4_1.c
@@ -1,41 +1,63 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int i, j, rows;
 
-    
     scanf("%d", &rows);
 
-    for (i = 1; i <= rows; i++) {
-        for (j = 1; j <= i; j++) {
-            if (rows % 2 == 0) {
-                if (i % 2 == 0) {
+    for (i = 1; i <= rows; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            if (rows % 2 == 0)
+            {
+                if (i % 2 == 0)
+                {
                     printf("*");
-                } else {
+                }
+                else
+                {
                     printf(" ");
                 }
-            } else {
-                if (i % 2 == 0) {
+            }
+            else
+            {
+                if (i % 2 == 0)
+                {
                     printf(" ");
-                } else {
+                }
+                else
+                {
                     printf("*");
                 }
             }
         }
         printf("\n");
     }
-    for (i = rows-1; i >= 1; i--) {
-        for (j = 1; j <= i; j++) {
-            if (rows % 2 == 0) {
-                if (i % 2 == 0) {
+    for (i = rows - 1; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            if (rows % 2 == 0)
+            {
+                if (i % 2 == 0)
+                {
                     printf("*");
-                } else {
+                }
+                else
+                {
                     printf("");
                 }
-            } else {
-                if (i % 2 == 0) {
+            }
+            else
+            {
+                if (i % 2 == 0)
+                {
                     printf("");
-                } else {
+                }
+                else
+                {
                     printf("*");
                 }
             }
--- structure/grader4-5/grader5__.c
+++ structure/grader4-5/grader5__.c
@@ -1,15 +1,13 @@
-#include<stdio.h>
-int main (){
-    
+#include <stdio.h>
+int main()
+{
+
     int m[1000];
-    int n,i;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
+    int n, i;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
     {
-        
     }
 
-    
-    
-    return 0 ;
+    return 0;
 }
