e864fc6d07f8f3871167fa5543b5babc21dbea3e
Gapraomoosap <peelasin2544@gmail.com>
2023-03-05 16:57:01 +0700

    update ingredients

--- Database/learn101/test/script.js
+++ Database/learn101/test/script.js
@@ -4,29 +4,28 @@ $(document).ready(function() {
     });
   });
   
-  
-
-  // Get the Delivery and Pick Up buttons
-  function updateDropdownOptions() {
-    // Get the dropdown and its options
-    alert("Hello!!");
-    // Set new options based on which button was clicked
-    if (buttonClicked == 1) {
-      alert("123");
-      // dropdown.setAttribute("title", "Number");
-      // options.add(new Option("1", "1"));
-      // options.add(new Option("2", "2"));
-      // options.add(new Option("3", "3"));
-    } else if (buttonClicked == 2) {
-      alert("456");
-      // dropdown.setAttribute("title", "Alfa");
-      // options.add(new Option("a", "a"));
-      // options.add(new Option("b", "b"));
-      // options.add(new Option("c", "c"));
+  function toggleButton() {
+    var button = document.getElementById("toggle-button");
+    if (button.classList.contains("button1")) {
+      button.innerHTML = "Add to Basket";
+      button.classList.remove("button1");
+      button.classList.add("button2");
+    } else {
+      window.location.href = "#";
     }
-    
-    // If the dropdown has no options, set the title to a default value
-    // if (options.length == 0) {
-    //   dropdown.setAttribute("title", "Select an option");
-    // }
   }
+
+  function IngredientsButton(){
+    var textContainer = document.getElementById("Ingredients-container");
+    var button = document.getElementById("toggle-button-ingredients");
+    if (textContainer.style.display === "none") {
+      textContainer.style.display = "block";
+     
+      
+    } else {
+      textContainer.style.display = "none";
+      
+    }
+
+  }
+  
