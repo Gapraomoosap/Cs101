b57bbd49d53626ca73b555137e0663a19d5ad25a
Gapraomoosap <peelasin2544@gmail.com>
2023-03-06 14:37:23 +0700

    Creat cart page

--- Database/learn101/test/script.js
+++ Database/learn101/test/script.js
@@ -4,28 +4,27 @@ $(document).ready(function() {
     });
   });
   
-  function toggleButton() {
-    var button = document.getElementById("toggle-button");
-    if (button.classList.contains("button1")) {
-      button.innerHTML = "Add to Basket";
-      button.classList.remove("button1");
-      button.classList.add("button2");
-    } else {
-      window.location.href = "#";
-    }
+function toggleButton() {
+  var button = document.getElementById("toggle-button");
+  if (button.classList.contains("button1")) {
+    button.innerHTML = "Add to Basket";
+    button.classList.remove("button1");
+    button.classList.add("button2");
+  } else {
+    window.location.href = "#";
   }
+}
 
-  function IngredientsButton(){
-    var textContainer = document.getElementById("Ingredients-container");
-    var button = document.getElementById("toggle-button-ingredients");
-    if (textContainer.style.display === "none") {
-      textContainer.style.display = "block";
+function IngredientsButton(){
+  var textContainer = document.getElementById("Ingredients-container");
+  var button = document.getElementById("toggle-button-ingredients");
+  if (textContainer.style.display === "none") {
+    textContainer.style.display = "block";
      
       
-    } else {
-      textContainer.style.display = "none";
+  } else {
+    textContainer.style.display = "none";
       
-    }
-
   }
-  
+
+}
