20686e5620b1dbc24bf5e2744bd6c28da2d2f49c
Gapraomoosap <peelasin2544@gmail.com>
2023-07-07 00:33:34 +0700

    update oop

--- year2/OOP/Main/src/Employee.java
+++ year2/OOP/Main/src/Employee.java
@@ -1,5 +1,17 @@
 class Employee {
-    private String id;
-    private String name;
-    private Double salary;
+    protected String id;
+    protected String name;
+    protected Double salary;
+    public void setID(String id)
+    {
+        this.id=id;
+    }
+    
+    public void setName(String name){
+        this.name = name ;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 }
