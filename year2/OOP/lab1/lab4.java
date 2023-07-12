66853a846bccc709f8975894aff221f00921e4bf
Gapraomoosap <116646446+Gapraomoosap@users.noreply.github.com>
2023-07-12 10:27:04 +0700

    Do lab4

--- year2/OOP/lab1/lab4.java
+++ year2/OOP/lab1/lab4.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class lab4{
     public static void main(String[] args)
     {
-        System.out.print("Hello");
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter balance and interest rate :");
+        double balance , annbalance , interest;
+        balance = sc.nextDouble();
+        annbalance = sc.nextDouble();
+        interest = balance * (annbalance / 1200);
+        System.out.printf("The interest is %.5f",interest);
+        sc.close();
     }
 }
