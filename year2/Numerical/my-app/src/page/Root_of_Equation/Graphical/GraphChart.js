9021f63d5244a78adb81215a66866f949fa3f5e8
Gapraomoosap <116646446+Gapraomoosap@users.noreply.github.com>
2023-11-08 15:24:11 +0700

    update graph and coin game

--- year2/Numerical/my-app/src/page/Root_of_Equation/Graphical/GraphChart.js
+++ year2/Numerical/my-app/src/page/Root_of_Equation/Graphical/GraphChart.js
@@ -1,48 +1,23 @@
 import { useEffect, useRef } from 'react';
 import { Chart } from 'chart.js/auto';
+import Plot from 'react-plotly.js';
 
 const ChartComponent = ({ data }) => {
-  const chartRef = useRef(null);
-  const chartInstance = useRef(null);
-
-  useEffect(() => {
-    // Destroy previous chart instance if it exists
-    if (chartInstance.current) {
-      chartInstance.current.destroy();
-    }
-
-    const ctx = chartRef.current.getContext('2d');
-    chartInstance.current = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: data.map(item => item.iteration),
-        datasets: [{
-          label: 'F(xi) vs Iteration',
-          data: data.map(item => item.Fxi),
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 1,
-        }]
-      },
-      options: {
-        scales: {
-          x: {
-            type: 'linear',
-            position: 'bottom'
-          }
-        }
-      }
-    });
-
-    // Cleanup function to destroy the chart instance when the component unmounts
-    return () => {
-      if (chartInstance.current) {
-        chartInstance.current.destroy();
-      }
-    };
-  }, [data]);
-
-  return <canvas ref={chartRef} />;
+  <>
+        <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+          },
+          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+        ]}
+        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+      />
+  </>
 };
 
 export default ChartComponent;
